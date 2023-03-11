33c09dc666e7733b692ce68fde49280e31a96c47
mmm <margaret.m.matson@gmail.com>
2023-03-11 13:58:34 -0600

    added server.js file

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 5000;
+}
+
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(routes);
+
+app.get("/static", (req, res) => {
+  res.render("static");
+});
+
+sequelize.sync({ force: false }).then(() => {
+    app.listen(port, () => console.log(`App listening on port http://localhost:${port}`));
+});
